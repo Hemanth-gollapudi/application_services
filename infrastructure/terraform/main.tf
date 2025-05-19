94b15bcec70030f13e848bdda4fbbf1a627877c6
Your Name <your.email@example.com>
2025-05-19 16:27:56 +0530

    commit

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -30,6 +30,19 @@ data "aws_vpc" "default" {
   default = true
 }
 
+# Get existing subnets in the VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Get the first available subnet
+data "aws_subnet" "default" {
+  id = tolist(data.aws_subnets.default.ids)[0]
+}
+
 # Create a subnet if none exists
 resource "aws_subnet" "app_subnet" {
   vpc_id                  = data.aws_vpc.default.id
@@ -51,13 +64,13 @@ resource "tls_private_key" "app_private_key" {
 # Store private key locally
 resource "local_file" "private_key" {
   content         = tls_private_key.app_private_key.private_key_pem
-  filename        = "${path.module}/${var.project_name}-key.pem"
+  filename        = "./application-services-key.pem"
   file_permission = "0600"
 }
 
 # Create a new key pair
 resource "aws_key_pair" "app_key_pair" {
-  key_name   = "${var.project_name}-key"
+  key_name   = "application-services-key"
   public_key = tls_private_key.app_private_key.public_key_openssh
 
   lifecycle {
@@ -67,7 +80,7 @@ resource "aws_key_pair" "app_key_pair" {
 
 # Security Group for Application
 resource "aws_security_group" "app_sg" {
-  name        = "${var.project_name}-sg"
+  name        = "application-services-sg"
   description = "Security group for application services"
   vpc_id      = data.aws_vpc.default.id
 
@@ -137,74 +150,63 @@ resource "aws_security_group" "app_sg" {
   tags = {
     Name = "${var.project_name}-sg"
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # EC2 Instance
 resource "aws_instance" "app_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.medium"
+  subnet_id     = data.aws_subnet.default.id
   key_name      = aws_key_pair.app_key_pair.key_name
 
-  subnet_id                   = aws_subnet.app_subnet.id
-  vpc_security_group_ids      = [aws_security_group.app_sg.id]
-  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   root_block_device {
     volume_size = 30
     volume_type = "gp3"
     encrypted   = true
-
     tags = {
-      Name = "${var.project_name}-root-volume"
+      Name = "application-services-root-volume"
     }
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              # Update system packages
-              apt-get update
-              apt-get upgrade -y
+  tags = {
+    Name    = "application-services-server"
+    Project = "application-services"
+  }
 
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
               # Install Docker
-              apt-get install -y docker.io
+              yum update -y
+              yum install -y docker
               systemctl start docker
               systemctl enable docker
+              usermod -aG docker ec2-user
 
               # Install Docker Compose
-              curl -L "https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
 
               # Install kubectl
               curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
               chmod +x kubectl
               mv kubectl /usr/local/bin/
-
-              # Clone application repository
-              git clone ${var.git_repo_url} /app
-              cd /app
-
-              # Set up environment
-              cp env.example .env
-
-              # Start services
-              docker-compose up -d
               EOF
-
-  tags = {
-    Name    = "${var.project_name}-server"
-    Project = var.project_name
-  }
+  )
 
   depends_on = [aws_key_pair.app_key_pair]
 }
 
 # Elastic IP for EC2 Instance
 resource "aws_eip" "app_eip" {
-  domain     = "vpc"
-  instance   = aws_instance.app_server.id
-  depends_on = [aws_instance.app_server]
-
+  instance = aws_instance.app_server.id
+  domain   = "vpc"
   tags = {
-    Name = "${var.project_name}-eip"
+    Name = "application-services-eip"
   }
 } 
--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance (Elastic IP)"
-  value       = aws_eip.app_eip.public_ip
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
 }
 
 output "instance_public_dns" {
@@ -9,18 +9,18 @@ output "instance_public_dns" {
 }
 
 output "elastic_ip" {
-  description = "Elastic IP address assigned to the instance"
+  description = "Elastic IP address attached to the EC2 instance"
   value       = aws_eip.app_eip.public_ip
 }
 
 output "application_url" {
-  description = "URL to access the application"
-  value       = "http://${aws_eip.app_eip.public_ip}:8009"
+  description = "URL for accessing the application"
+  value       = "http://${aws_eip.app_eip.public_ip}:${var.app_port}"
 }
 
 output "keycloak_url" {
-  description = "URL to access Keycloak"
-  value       = "http://${aws_eip.app_eip.public_ip}:8084"
+  description = "URL for accessing Keycloak"
+  value       = "http://${aws_eip.app_eip.public_ip}:${var.keycloak_port}"
 }
 
 output "private_key_path" {
@@ -30,20 +30,20 @@ output "private_key_path" {
 
 output "ssh_command" {
   description = "SSH command to connect to the instance"
-  value       = "ssh -i ${local_file.private_key.filename} ubuntu@${aws_eip.app_eip.public_ip}"
+  value       = "ssh -i ${local_file.private_key.filename} ec2-user@${aws_eip.app_eip.public_ip}"
 }
 
 output "vpc_id" {
-  description = "ID of the VPC used"
+  description = "ID of the VPC"
   value       = data.aws_vpc.default.id
 }
 
 output "subnet_id" {
-  description = "ID of the subnet used"
-  value       = aws_subnet.app_subnet.id
+  description = "ID of the subnet"
+  value       = data.aws_subnet.default.id
 }
 
 output "availability_zone" {
-  description = "Availability zone used"
-  value       = aws_subnet.app_subnet.availability_zone
+  description = "Availability zone of the subnet"
+  value       = data.aws_subnet.default.availability_zone
 } 
